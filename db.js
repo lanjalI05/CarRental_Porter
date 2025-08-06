1ebb535f4d7842f8108191f85471525944678ec4
lanjalii05 <anjalirana05@gmail.com>
2025-08-06 18:00:55 +0530

    Delete All Files

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Module = require('node:module');
-mongoose.connect('mongodb:///localhost:27017//workshop')
-    .then(()=>{
-    console.log('Database Connection Successful');
-});
-
-module.exports = mongoose;
